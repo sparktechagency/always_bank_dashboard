7c3c00a35c0c95a7b5f18f808a8b0423fbbf1eba
foisaluddin400 <foisalrk2@gmail.com>
2025-01-04 12:28:01 +0600

    update

--- src/Pages/FAQ/FAQ.jsx
+++ src/Pages/FAQ/FAQ.jsx
@@ -33,7 +33,7 @@ const FAQ = () => {
   //     answer: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal ."
   //   },
   //   {
-  //     question: 'How do I access my medical records?',
+  //     questio: 'How do I access my medical records?',
   //     answer: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal ."
   //   },
   // ]
